604394d47168ffdf4dd4b977d8279b90ba7c79c3
FIQTOR <117913964+FIQTOR@users.noreply.github.com>
2023-01-22 18:30:51 +0700

    Add files via upload

---
+++ Slitherio_#01/Assets/script/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float rotationSensitivity;
+    public float movementSpeed;
+
+    private Vector3 mousePos;
+
+    void FixedUpdate()
+    {
+        RotateToMouse();
+        Movement();
+        BodyMovement();
+
+        if(Input.GetKey(KeyCode.F))
+            AddBodyPart();
+    }
+
+    void RotateToMouse()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Quaternion direction = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotationSensitivity * Time.deltaTime);
+    }
+
+    void Movement()
+    {
+        transform.position += transform.up * movementSpeed * Time.deltaTime;
+    }
+}
